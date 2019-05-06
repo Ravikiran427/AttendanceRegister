c7284d61e73ac8ed00f655d2b7873164f3260854
Ravikiran427 <ravikirantalawar@gmail.com>
2019-05-06 18:44:41 +0530

    trial1

--- app/src/main/java/com/example/attendanceregister/MainActivity.java
+++ app/src/main/java/com/example/attendanceregister/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.attendanceregister;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int Login=0;
+
+        if(Login==0)
+        {
+            startActivity(new Intent(this,unregistered.class).putExtra(Login));
+        }
+
+        else
+        {
+            startActivity(new Intent(this,registered.class));
+        }
     }
 }
